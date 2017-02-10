3b1daa0f3a8f41fd685f470a3f2db7ca57857862
Sean Griffin <sean@seantheprogrammer.com>
2017-02-10 12:22:02 -0500

    Fall back to `mysql_client` if `pkg-config` fails

--- build.rs
+++ build.rs
@@ -1,5 +1,24 @@
 extern crate pkg_config;
 
+use std::process::Command;
+
 fn main() {
-    pkg_config::probe_library("mysqlclient").unwrap();
+    if pkg_config::probe_library("mysqlclient").is_ok() {
+        // pkg_config did everything for us
+    } else {
+        if let Some(path) = mysql_config_output() {
+            println!("cargo:rustc-link-search=native={}", path);
+            println!("cargo:rustc-link-lib=mysqlclient");
+        }
+    }
+}
+
+fn mysql_config_output() -> Option<String> {
+    Command::new("mysql_config")
+        .arg("--variable=pkglibdir")
+        .output()
+        .into_iter()
+        .filter(|output| output.status.success())
+        .flat_map(|output| String::from_utf8(output.stdout).ok())
+        .next()
 }
