692e8ace5c7aa3ee9398c8876fbaca8ee1fd45e4
Sean Griffin <sean@seantheprogrammer.com>
2017-02-16 09:06:36 -0500

    Generate bindgen programatically
    Bindgen now recommends using a build script to generate the bindings,
    rather than including the file directly. I believe this is because it
    now includes system specific defines such as `__darwin_*` from `#include
    <stdio.h>`
    
    We have to do some additional legwork to get bindgen to work here (at
    least on my machine). On my setup (macOS, mysql installed via homebrew),
    `mysql.h` lives at `/usr/local/lib/mysql/mysql.h`. It has `#include
    "mysql/client_plugin.h"`, but that file then tries to include a local
    relative file as `#include <mysql/client_auth_shared.h>`
    
    So we need to go through some hoops to find the right directory, and
    pass it to clang (which admittedly, is probably a good idea to begin
    with).

--- build.rs
+++ build.rs
@@ -1,24 +1,49 @@
 extern crate pkg_config;
+extern crate bindgen;
 
+use bindgen::Builder;
+use std::env;
+use std::path::Path;
 use std::process::Command;
 
 fn main() {
+    generate_bindgen_file();
+
     if pkg_config::probe_library("mysqlclient").is_ok() {
         // pkg_config did everything for us
     } else {
-        if let Some(path) = mysql_config_output() {
+        if let Some(path) = mysql_config_variable("pkglibdir") {
             println!("cargo:rustc-link-search=native={}", path);
             println!("cargo:rustc-link-lib=mysqlclient");
         }
     }
 }
 
-fn mysql_config_output() -> Option<String> {
+fn mysql_config_variable(var_name: &str) -> Option<String> {
     Command::new("mysql_config")
-        .arg("--variable=pkglibdir")
+        .arg(format!("--variable={}", var_name))
         .output()
         .into_iter()
         .filter(|output| output.status.success())
         .flat_map(|output| String::from_utf8(output.stdout).ok())
         .next()
+}
+
+fn generate_bindgen_file() {
+    let out_dir = env::var("OUT_DIR").unwrap();
+    Builder::default()
+        .no_unstable_rust()
+        .header("wrapper.h")
+        .clang_arg(format!("-I{}", mysql_include_dir()))
+        .generate()
+        .expect("Unable to generate bindings for libmysqlclient")
+        .write_to_file(Path::new(&out_dir).join("bindings.rs"))
+        .expect("Unable to write bindings to file");
+}
+
+fn mysql_include_dir() -> String {
+    pkg_config::get_variable("mysqlclient", "includedir")
+        .ok()
+        .or_else(|| mysql_config_variable("pkgincludedir"))
+        .expect("Unable to locate `mysql.h`")
 }
