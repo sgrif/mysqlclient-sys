f8e60be2877b62e08479a1b6b0486eb48f98fdba
Sean Griffin <sean@seantheprogrammer.com>
2017-02-19 08:05:32 -0500

    Release v0.2.0
    This release changes from generating the bindings at build time to
    vendoring pre-generated bindings. There were too many problems caused by
    requiring bleeding edge clang, and it's been the source of many bug
    reports. We may explore that option again in the future (maybe behind a
    cargo feature)

--- build.rs
+++ build.rs
@@ -1,14 +1,8 @@
 extern crate pkg_config;
-extern crate bindgen;
 
-use bindgen::Builder;
-use std::env;
-use std::path::Path;
 use std::process::Command;
 
 fn main() {
-    generate_bindgen_file();
-
     if pkg_config::probe_library("mysqlclient").is_ok() {
         // pkg_config did everything for us
     } else {
@@ -28,26 +22,4 @@ fn mysql_config_variable(var_name: &str) -> Option<String> {
         .flat_map(|output| String::from_utf8(output.stdout).ok())
         .map(|output| output.trim().to_string())
         .next()
-}
-
-fn generate_bindgen_file() {
-    let out_dir = env::var("OUT_DIR").unwrap();
-    Builder::default()
-        .no_unstable_rust()
-        .header("wrapper.h")
-        .clang_arg(format!("-I{}", mysql_include_dir()))
-        .whitelisted_function("mysql.*")
-        .whitelisted_type("MYSQL.*")
-        .whitelisted_var("MYSQL.*")
-        .generate()
-        .expect("Unable to generate bindings for libmysqlclient")
-        .write_to_file(Path::new(&out_dir).join("bindings.rs"))
-        .expect("Unable to write bindings to file");
-}
-
-fn mysql_include_dir() -> String {
-    pkg_config::get_variable("mysqlclient", "includedir")
-        .ok()
-        .or_else(|| mysql_config_variable("pkgincludedir"))
-        .expect("Unable to locate `mysql.h`")
 }
