d593ea9f2988f860cc63bd346e7136da2df6beba
Georg Semmler <georg.semmler@giga-infosystems.com>
2024-04-19 14:32:51 +0200

    Update bundled bindings to support several different platforms and mysqlclient versions

--- build.rs
+++ build.rs
@@ -1,42 +1,150 @@
-extern crate pkg_config;
-
-#[cfg(target_env = "msvc")]
-extern crate vcpkg;
-
 use std::env;
 use std::process::Command;
 
 fn main() {
-    if pkg_config::probe_library("mysqlclient").is_ok() {
-        // pkg_config did everything for us
-        return
+    let target = std::env::var("TARGET")
+        .expect("Set by cargo")
+        .to_ascii_uppercase()
+        .replace("-", "_");
+    println!("cargo::rerun-if-env-changed=MYSQLCLIENT_VERSION");
+    println!("cargo::rerun-if-env-changed=MYSQLCLIENT_LIB");
+    println!("cargo::rerun-if-env-changed=MYSQLCLIENT_STATIC");
+    println!("cargo::rerun-if-env-changed=MYSQLCLIENT_VERSION_{target}");
+    println!("cargo::rerun-if-env-changed=MYSQLCLIENT_LIB_{target}");
+    println!("cargo::rerun-if-env-changed=MYSQLCLIENT_STATIC_{target}");
+    let link_specifier = if env::var("MYSQLCLIENT_STATIC")
+        .or(env::var(format!("MYSQLCLIENT_STATIC_{target}")))
+        .is_ok()
+    {
+        "static="
+    } else {
+        ""
+    };
+    if let Ok(lib) = pkg_config::probe_library("mysqlclient") {
+        // pkg_config did everything but the version flags for us
+        parse_version(&lib.version);
+        return;
+    } else if let Ok(lib) = pkg_config::probe_library("mariadb") {
+        // pkg_config did everything but the version flags for us
+        parse_version(&lib.version);
+        return;
     } else if try_vcpkg() {
         // vcpkg did everything for us
-        return;
-    } else if let Ok(path) = env::var("MYSQLCLIENT_LIB_DIR") {
-        println!("cargo:rustc-link-search=native={}", path);
-    } else if let Some(path) = mysql_config_variable("pkglibdir") {
-        println!("cargo:rustc-link-search=native={}", path);
+        if let Ok(version) =
+            env::var("MYSQLCLIENT_VERSION").or(env::var(format!("MYSQLCLIENT_VERSION_{target}")))
+        {
+            parse_version(&version);
+            return;
+        }
+    } else if let Ok(path) =
+        env::var("MYSQLCLIENT_LIB_DIR").or(env::var(format!("MYSQLCLIENT_LIB_DIR_{target}")))
+    {
+        println!("cargo:rustc-link-search=native={path}");
+        println!("cargo:rustc-link-lib={link_specifier}mysqlclient");
+        if let Ok(version) =
+            env::var("MYSQLCLIENT_VERSION").or(env::var(format!("MYSQLCLIENT_VERSION_{target}")))
+        {
+            parse_version(&version);
+            return;
+        }
+    } else if let Some(output) = mysql_config_variable("--libs") {
+        let parts = output.split_ascii_whitespace().collect::<Vec<_>>();
+        assert_eq!(
+            parts.len(),
+            2,
+            "Unexpected output from mysql_config: `{output}`"
+        );
+        let lib = parts[1]
+            .strip_prefix("-l")
+            .unwrap_or_else(|| panic!("Unexpected output from mysql_config: {output}"));
+        let path = parts[0]
+            .strip_prefix("-L")
+            .unwrap_or_else(|| panic!("Unexpected output from mysql_config: {output}"));
+        println!("cargo:rustc-link-search=native={path}");
+        println!("cargo:rustc-link-lib={link_specifier}{lib}");
+        if let Some(version) = mysql_config_variable("--version") {
+            parse_version(&version);
+            return;
+        }
     }
-
-    if cfg!(all(windows, target_env="gnu")) {
-        println!("cargo:rustc-link-lib=dylib=mysql");
-    } else if cfg!(all(windows, target_env="msvc")) {
-        println!("cargo:rustc-link-lib=static=mysqlclient");
-    } else {
-        println!("cargo:rustc-link-lib=mysqlclient");
-    }
+    panic!(
+        "Did not find a compatible version of libmysqlclient.\n\
+            Ensure that you installed one and teached mysqlclient-sys how to find it\n\
+            You have the following options for that:\n\
+            \n\
+            * Use `pkg_config` to automatically detect the right location\n\
+            * Use vcpkg to automatically detect the right location. \n\
+              You also need to set `MYSQLCLIENT_VERSION` to specify which\n\
+              version of libmysqlclient you are using\n\
+            * Set the `MYSQLCLIENT_LIB_DIR` and `MYSQLCLIENT_VERSION` environment \n\
+              variables to point the compiler to the right directory and specify \n\
+              which version is used\n\
+            * Make the `mysql_config` binary avaible in the environment that invokes\n\
+              the compiler"
+    );
 }
 
 fn mysql_config_variable(var_name: &str) -> Option<String> {
     Command::new("mysql_config")
-        .arg(format!("--variable={}", var_name))
+        .arg(var_name)
         .output()
         .into_iter()
         .filter(|output| output.status.success())
         .flat_map(|output| String::from_utf8(output.stdout).ok())
         .map(|output| output.trim().to_string())
         .next()
+}
+
+fn parse_version(version: &str) {
+    // we have bundled bindings for:
+    // * x86_64 for windows, linux and macos
+    // * x86 for windows
+    // * aarch64 for linux + macos
+    if !(matches!(
+        std::env::var("CARGO_CFG_TARGET_ARCH").as_deref(),
+        Ok("x86_64")
+    ) || (std::env::var("CARGO_CFG_WINDOWS").is_ok()
+        && matches!(std::env::var("CARGO_CFG_TARGET_ARCH").as_deref(), Ok("x86")))
+        || std::env::var("CARGO_CFG_WINDOWS").is_err()
+            && matches!(
+                std::env::var("CARGO_CFG_TARGET_ARCH").as_deref(),
+                Ok("aarch64")
+            ))
+    {
+        panic!(
+            "mysqlclient-sys does not provide bundled bindings for {}\n\
+                Consider using the `buildtime_bindgen` feature or \
+                contribute bindings for your target to the crate",
+            std::env::var("TARGET").expect("Set by cargo")
+        );
+    }
+    // ubuntu/debian packages use the following package versions:
+    // libmysqlclient20 -> 5.7.x
+    // libmysqlclient21 -> 8.0.x
+    // libmysqlclient23 -> 8.3.0
+    if version.starts_with("5.7.") || version.starts_with("20.") {
+        if std::env::var("CARGO_CFG_WINDOWS").is_ok() {
+            panic!(
+                "mysqlclient-sys does not provide bundled bindings \
+                 for libmysqlclient {version} on windows. \n\
+                 Consider using the `buildtime_bindgen` feature or \
+                 contribute bindings to the crate"
+            );
+        }
+        println!("cargo:rustc-cfg=mysql_5_7_x");
+    } else if version.starts_with("8.0.") || version.starts_with("21.") {
+        println!("cargo:rustc-cfg=mysql_8_0_x");
+    } else if version.starts_with("8.3.") || version.starts_with("23.") {
+        println!("cargo:rustc-cfg=mysql_8_3_x");
+    } else if version.starts_with("10.") || version.starts_with("11.") {
+        println!("cargo:rustc-cfg=mariadb_10_x");
+    } else {
+        panic!(
+            "mysqlclient-sys does not provide bundled bindings for libmysqlclient {version}. \
+             Consider using the `buildtime_bindgen` feature or \
+             contribute bindings to the crate"
+        )
+    }
 }
 
 #[cfg(target_env = "msvc")]
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,96 @@
 #![allow(non_camel_case_types, non_snake_case, non_upper_case_globals)]
 
-#[cfg(not(windows))]
-include!("bindings_macos.rs");
+#[allow(dead_code)]
+mod bindings {
+    #[cfg(all(
+        mysql_5_7_x,
+        not(target_os = "windows"),
+        any(target_arch = "x86_64", target_arch = "aarch64")
+    ))]
+    include!("../bindings/bindings_5_7_42_x86_64_linux.rs");
 
-#[cfg(windows)]
-include!("bindings_windows.rs");
+    #[cfg(all(
+        mysql_8_0_x,
+        not(target_os = "windows"),
+        any(target_arch = "x86_64", target_arch = "aarch64")
+    ))]
+    include!("../bindings/bindings_8_0_36_x86_64_linux.rs");
+
+    #[cfg(all(
+        mysql_8_3_x,
+        not(target_os = "windows"),
+        any(target_arch = "x86_64", target_arch = "aarch64")
+    ))]
+    include!("../bindings/bindings_8_3_0_x86_64_linux.rs");
+
+    #[cfg(all(
+        mariadb_10_x,
+        not(target_os = "windows"),
+        any(target_arch = "x86_64", target_arch = "aarch64")
+    ))]
+    include!("../bindings/bindings_mariadb_10_11_x86_64_linux.rs");
+
+    #[cfg(all(mysql_8_0_x, target_os = "windows", target_arch = "x86_64"))]
+    include!("../bindings/bindings_8_0_36_x86_64_windows.rs");
+
+    #[cfg(all(mysql_8_0_x, target_os = "windows", target_arch = "x86"))]
+    include!("../bindings/bindings_8_0_36_i868_windows.rs");
+
+    #[cfg(all(mysql_8_3_x, target_os = "windows", target_arch = "x86_64"))]
+    include!("../bindings/bindings_8_3_0_x86_64_windows.rs");
+
+    #[cfg(all(mysql_8_3_x, target_os = "windows", target_arch = "x86"))]
+    include!("../bindings/bindings_8_3_0_i868_windows.rs");
+
+    #[cfg(all(mariadb_10_x, target_os = "windows", target_arch = "x86_64"))]
+    include!("../bindings/bindings_mariadb_10_11_x86_64_windows.rs");
+
+    #[cfg(all(mariadb_10_x, target_os = "windows", target_arch = "x86"))]
+    include!("../bindings/bindings_mariadb_10_11_i686_windows.rs");
+}
+
+pub use self::bindings::*;
+
+// the following type devs are compatibility shims for diesel
+
+#[cfg(all(not(mysql_5_7_x), not(mariadb_10_x)))]
+pub type my_bool = bool;
+
+#[cfg(all(not(mysql_5_7_x), not(mariadb_10_x)))]
+pub const FALSE: my_bool = false;
+#[cfg(all(not(mysql_5_7_x), not(mariadb_10_x)))]
+pub const TRUE: my_bool = true;
+
+#[cfg(any(mysql_5_7_x, mariadb_10_x))]
+pub const FALSE: my_bool = 0;
+
+#[cfg(any(mysql_5_7_x, mariadb_10_x))]
+pub const TRUE: my_bool = 1;
+
+pub const SUPPORTS_MYSQL_SSL_MODE: bool = !cfg!(mariadb_10_x);
+
+#[cfg(mariadb_10_x)]
+#[repr(u32)]
+#[derive(Debug, Copy, Clone, Hash, PartialEq, Eq)]
+pub enum mysql_ssl_mode {
+    SSL_MODE_DISABLED = 1,
+    SSL_MODE_PREFERRED = 2,
+    SSL_MODE_REQUIRED = 3,
+    SSL_MODE_VERIFY_CA = 4,
+    SSL_MODE_VERIFY_IDENTITY = 5,
+}
+
+#[cfg(mariadb_10_x)]
+pub mod mysql_option {
+    /// that's not supported, do not use it
+    pub const MYSQL_OPT_SSL_MODE: crate::bindings::mysql_option =
+        crate::bindings::mysql_option::MYSQL_SERVER_PUBLIC_KEY;
+    pub const MYSQL_OPT_SSL_CA: crate::bindings::mysql_option =
+        crate::bindings::mysql_option::MYSQL_OPT_SSL_CA;
+    pub const MYSQL_OPT_SSL_CERT: crate::bindings::mysql_option =
+        crate::bindings::mysql_option::MYSQL_OPT_SSL_CERT;
+    pub const MYSQL_OPT_SSL_KEY: crate::bindings::mysql_option =
+        crate::bindings::mysql_option::MYSQL_OPT_SSL_KEY;
+    pub const MYSQL_SET_CHARSET_NAME: crate::bindings::mysql_option =
+        crate::bindings::mysql_option::MYSQL_SET_CHARSET_NAME;
+}
